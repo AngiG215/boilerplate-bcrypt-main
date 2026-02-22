4caad943bdc3c9bf53871d1633a090b08fc5c9d4
Angela <angieadrigarcia572@gmail.com>
2026-02-22 00:19:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 const bcrypt = require('bcrypt');
 
+// Definimos la contraseña a ocultar y el costo (saltRounds)
+const myPlaintextPassword = 'password123';
+const saltRounds = 12;
+
+// Generamos el hash
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Tu hash generado es: ' + hash);
+    // Aquí es donde normalmente guardarías el 'hash' en tu base de datos
+  }
+});
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
 
