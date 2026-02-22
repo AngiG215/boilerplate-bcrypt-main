3d18ae0cd18d9a28a9eb513207f2a710f53eb9f5
Angela <angieadrigarcia572@gmail.com>
2026-02-22 00:31:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
 fccTesting(app);
-const saltRounds = 12;
-const myPlaintextPassword = 'sUperpassw0rd!';
+
 const someOtherPlaintextPassword = 'pass123';
 const bcrypt = require('bcrypt');
 
